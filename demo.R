46fa1d1615adc870ae1edfc9d6eb1d35761acc79
FredCommo <fredcommo@gmail.com>
2014-03-25 20:10:49 -0700

    new demo using github files

--- demo.R
+++ demo.R
@@ -1,8 +1,9 @@
 # Demo
-require('devtools')
-require('rGithubClient')
+require(devtools)
+require(rGithubClient)
+require(RCurl)
 
-getFilesList <- function(git, tag = ''){
+getFilesList <- function(git, tag=""){
   flist <- git@tree$path
   return(flist[grep(tag, flist)])
 }
@@ -11,53 +12,26 @@ git <- getRepo('fredcommo/IC50new')
 Rlist <- getFilesList(git, '[^Demo].R$')
 sourceRepoFile(git, Rlist)
 
-# demo1: no replicate
-set.seed(12345)
-n <- 8
-x <- sapply(seq(-3, 1, len=8), function(p) 10^(p))
-bottom=0; top=1; xmid=(min(log10(x)) + max(log10(x)))/2; scal=-1; s=1
-y <- .PL5(bottom, top, xmid, scal, s, log10(x)) + rnorm(n, 0, .05)
+# demo1: data without replicates
+url <- getURL("https://raw.github.com/fredcommo/IC50new/master/demo_files/demo1.csv")
+dat <- read.delim(text=url)
+demo1 <- nplm(dat$x, dat$y)
+plot(demo1)
+getAUC(demo1)
+getEstimates(demo1)
 
-test <- Logistic(x, y)
-test
-plot(test)
-getAUC(test)
-getEstimates(test)
-getPar(test)
-predict(test, c(.25, .5, .75))
+# demo2: data with replicates
+url <- getURL("https://raw.github.com/fredcommo/IC50new/master/demo_files/demo2.csv")
+dat <- read.delim(text=url)
+demo2 <- nplm(dat$x, dat$y)
+plot(demo2)
+getAUC(demo2)
+getEstimates(demo2)
 
-# demo2: with replicates and outliers
-set.seed(12345)
-n = 8; replic = 3
-x <- sapply(seq(-3, 1, len=n), function(p) 10^(p))
-x <- rep(x, replic)
-bottom=0; top=1; xmid=(min(log10(x)) + max(log10(x)))/2; scal=-1; s=1
-
-y <- .PL5(bottom, top, xmid, scal, s, log10(x)) + rnorm(n*replic, 0, .1)
-y[2]<- y[2]*.65; y[7]<- y[7] + .4
-
-test <- Logistic(x, y)
-test
-plot(test)
-predict(test, c(.1, .25, .5, .75, .9))
-
-estims <- getEstimates(test)
-plot(estims$Surv, estims$D)
-pad = .025
-segments(x0=rep(estims$Surv-pad, 2), x1=rep(estims$Surv+pad, 2), y0=c(estims$Dmin, estims$Dmax))
-segments(x0=estims$Surv, y0=estims$Dmin, y1=estims$Dmax)
-
-# demo3: proliferation curve
-set.seed(12346)
-n = 8; replic = 3
-x <- rep(seq(1, 48, len=n), replic)
-bottom=0; top=1; xmid=(min(x) + max(x))/2; scal=0.1; s=.5
-
-y <- .PL5(bottom, top, xmid, scal, s, x) + rnorm(n*replic, 0, .05)
-
-test <- Logistic(x, y, isProp=FALSE, useLog=FALSE)
-test
-
-plot(test, xlab = "Time (hrs)", ylab = "Proliferation", showInfl=TRUE, showTarget=FALSE)
-getInflexion(test)
-predict(test, c(.1, .25, .5))
+# demo3: proliferation test with replicates
+url <- getURL("https://raw.github.com/fredcommo/IC50new/master/demo_files/demo3.csv")
+dat <- read.delim(text=url)
+demo3 <- nplm(dat$x, dat$y, useLog=FALSE)
+plot(demo3, showInf=TRUE, showTarget=FALSE)
+getEstimates(demo3)
+predict(demo3, .8)
